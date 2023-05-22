cea98cafcdc1fe5f691463e637fe8c70c6f4c16a
David Härdeman <david@hardeman.nu>
2023-05-22 13:46:04 +0200

    Module: Improve error handling in LDAPmessage_to_python

--- Modules/message.h
+++ Modules/message.h
@@ -3,9 +3,11 @@
 #ifndef __h_message
 #define __h_message
 
+#include <stdbool.h>
+
 #include "common.h"
 
-extern PyObject *LDAPmessage_to_python(LDAP *ld, LDAPMessage *m, int add_ctrls,
-                                       int add_intermediates);
+extern PyObject *LDAPmessage_to_python(LDAP *ld, LDAPMessage *m, bool add_ctrls,
+                                       bool add_intermediates);
 
 #endif /* __h_message_ */
