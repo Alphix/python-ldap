51d65158ae1fff7443849ceb3b0530197c494de3
David Härdeman <david@hardeman.nu>
2023-05-22 11:25:14 +0200

    Module: Improve error handling in entry parsing
    Move the entry parsing into a separate subfunction and use
    Linux kernel coding style to handle errors. Despite adding several
    new checks (the return value of every function call is now
    checked), the end result is slightly fewer lines and a more
    readable main flow through each function.

--- Modules/message.c
+++ Modules/message.c
@@ -1,6 +1,138 @@
 /* See https://www.python-ldap.org/ for details. */
 
 #include "pythonldap.h"
+#include <stdbool.h>
+
+static int
+process_entry_attribute(LDAP *ld, LDAPMessage *entry, PyObject *attrdict,
+                        const char *attr, BerElement *ber)
+{
+    PyObject *pyattr;
+    struct berval **bvals;
+    PyObject *valuelist;
+    PyObject *valuestr = NULL;
+    int result = -1;
+
+    pyattr = PyUnicode_FromString(attr);
+    if (!pyattr)
+        goto out;
+
+    bvals = ldap_get_values_len(ld, entry, attr);
+    if (!bvals) {
+        LDAPerror(ld);
+        goto out_pyattr;
+    }
+
+    /* Find the list to append to */
+    valuelist = PyDict_GetItem(attrdict, pyattr);
+    if (valuelist) {
+        /*
+         * Multiple attribute entries with same name. This code path
+         * is rarely used and cannot be exhausted with OpenLDAP
+         * tests. 389-DS sometimes triggeres it, see
+         * https://github.com/python-ldap/python-ldap/issues/218
+         */
+        Py_INCREF(valuelist);
+    } else {
+        valuelist = PyList_New(0);
+
+        if (!valuelist)
+            goto out_bvals;
+
+        if (PyDict_SetItem(attrdict, pyattr, valuelist) < 0)
+            goto out_valuelist;
+    }
+
+    for (int i = 0; bvals[i]; i++) {
+        valuestr = LDAPberval_to_object(bvals[i]);
+        if (!valuestr)
+            goto out_valuelist;
+
+        if (PyList_Append(valuelist, valuestr) < 0)
+            goto out_valuestr;
+
+        Py_DECREF(valuestr);
+        valuestr = NULL;
+    }
+
+    result = 0;
+
+out_valuestr:
+    Py_XDECREF(valuestr);
+out_valuelist:
+    Py_DECREF(valuelist);
+out_bvals:
+    ldap_value_free_len(bvals);
+out_pyattr:
+    Py_XDECREF(pyattr);
+out:
+    return result;
+}
+
+static PyObject *
+process_entry(LDAP *ld, LDAPMessage *entry, bool add_ctrls)
+{
+    char *dn;
+    PyObject *pydn = NULL;
+    PyObject *attrdict = NULL;
+    BerElement *ber = NULL;
+    LDAPControl **serverctrls = NULL;
+    PyObject *pyctrls = NULL;
+    PyObject *result = NULL;
+
+    dn = ldap_get_dn(ld, entry);
+    if (!dn) {
+        LDAPerror(ld);
+        goto out;
+    }
+
+    pydn = PyUnicode_FromString(dn);
+    if (!pydn)
+        goto out_dn;
+
+    attrdict = PyDict_New();
+    if (!attrdict)
+        goto out_dn;
+
+    /* FIXME: could use ldap_get_attribute_ber here? */
+    for (const char *attr = ldap_first_attribute(ld, entry, &ber); attr;
+         attr = ldap_next_attribute(ld, entry, ber)) {
+        if (process_entry_attribute(ld, entry, attrdict, attr, ber) < 0)
+		    goto out_ber;
+    }
+
+    if (add_ctrls) {
+        if (ldap_get_entry_controls(ld, entry, &serverctrls) != LDAP_SUCCESS) {
+            LDAPerror(ld);
+            goto out_ber;
+        }
+
+        pyctrls = LDAPControls_to_List(serverctrls);
+        if (!pyctrls) {
+            int err = LDAP_NO_MEMORY;
+            /* FIXME: missing LDAP_BEGIN/END_ALLOW_THREADS? */
+            ldap_set_option(ld, LDAP_OPT_ERROR_NUMBER, &err);
+            LDAPerror(ld);
+            goto out_ctrls;
+        }
+
+        result = Py_BuildValue("(OOO)", pydn, attrdict, pyctrls);
+    } else {
+        result = Py_BuildValue("(OO)", pydn, attrdict);
+    }
+
+out_ctrls:
+    Py_XDECREF(pyctrls);
+    ldap_controls_free(serverctrls);
+out_ber:
+    ber_free(ber, 0);
+    Py_XDECREF(attrdict);
+out_dn:
+    Py_XDECREF(pydn);
+    ldap_memfree(dn);
+out:
+    return result;
+}
 
 /*
  * Converts an LDAP message into a Python structure.
@@ -30,15 +162,9 @@ PyObject *
 LDAPmessage_to_python(LDAP *ld, LDAPMessage *m, int add_ctrls,
                       int add_intermediates)
 {
-    /* we convert an LDAP message into a python structure.
-     * It is always a list of dictionaries.
-     * We always free m.
-     */
-
     PyObject *result, *pyctrls = NULL;
     LDAPMessage *entry;
     LDAPControl **serverctrls = NULL;
-    int rc;
 
     result = PyList_New(0);
     if (result == NULL) {
@@ -46,151 +172,25 @@ LDAPmessage_to_python(LDAP *ld, LDAPMessage *m, int add_ctrls,
         return NULL;
     }
 
-    for (entry = ldap_first_entry(ld, m);
-         entry != NULL; entry = ldap_next_entry(ld, entry)) {
-        char *dn;
-        char *attr;
-        BerElement *ber = NULL;
-        PyObject *entrytuple;
-        PyObject *attrdict;
-        PyObject *pydn;
-
-        dn = ldap_get_dn(ld, entry);
-        if (dn == NULL) {
+    for (entry = ldap_first_entry(ld, m); entry;
+         entry = ldap_next_entry(ld, entry)) {
+        PyObject *entrytuple = process_entry(ld, entry, add_ctrls != 0);
+        if (!entrytuple) {
             Py_DECREF(result);
             ldap_msgfree(m);
-            return LDAPerror(ld);
-        }
-
-        attrdict = PyDict_New();
-        if (attrdict == NULL) {
-            Py_DECREF(result);
-            ldap_msgfree(m);
-            ldap_memfree(dn);
             return NULL;
         }
 
-        rc = ldap_get_entry_controls(ld, entry, &serverctrls);
-        if (rc) {
+        if (PyList_Append(result, entrytuple) < 0) {
+            Py_DECREF(entrytuple);
             Py_DECREF(result);
             ldap_msgfree(m);
-            ldap_memfree(dn);
-            return LDAPerror(ld);
-        }
-
-        /* convert serverctrls to list of tuples */
-        if (!(pyctrls = LDAPControls_to_List(serverctrls))) {
-            int err = LDAP_NO_MEMORY;
-
-            ldap_set_option(ld, LDAP_OPT_ERROR_NUMBER, &err);
-            Py_DECREF(result);
-            ldap_msgfree(m);
-            ldap_memfree(dn);
-            ldap_controls_free(serverctrls);
-            return LDAPerror(ld);
-        }
-        ldap_controls_free(serverctrls);
-
-        /* Fill attrdict with lists */
-        for (attr = ldap_first_attribute(ld, entry, &ber);
-             attr != NULL; attr = ldap_next_attribute(ld, entry, ber)
-            ) {
-            PyObject *valuelist;
-            PyObject *pyattr;
-            struct berval **bvals;
-
-            pyattr = PyUnicode_FromString(attr);
-
-            bvals = ldap_get_values_len(ld, entry, attr);
-
-            /* Find which list to append to */
-            if (PyDict_Contains(attrdict, pyattr)) {
-                /* Multiple attribute entries with same name. This code path
-                 * is rarely used and cannot be exhausted with OpenLDAP
-                 * tests. 389-DS sometimes triggeres it, see
-                 * https://github.com/python-ldap/python-ldap/issues/218
-                 */
-                valuelist = PyDict_GetItem(attrdict, pyattr);
-                /* Turn borrowed reference into owned reference */
-                if (valuelist != NULL)
-                    Py_INCREF(valuelist);
-            }
-            else {
-                valuelist = PyList_New(0);
-                if (valuelist != NULL && PyDict_SetItem(attrdict,
-                                                        pyattr,
-                                                        valuelist) == -1) {
-                    Py_DECREF(valuelist);
-                    valuelist = NULL;   /* catch error later */
-                }
-            }
-
-            if (valuelist == NULL) {
-                Py_DECREF(pyattr);
-                Py_DECREF(attrdict);
-                Py_DECREF(result);
-                if (ber != NULL)
-                    ber_free(ber, 0);
-                ldap_msgfree(m);
-                ldap_memfree(attr);
-                ldap_memfree(dn);
-                Py_XDECREF(pyctrls);
-                return NULL;
-            }
-
-            if (bvals != NULL) {
-                Py_ssize_t i;
-
-                for (i = 0; bvals[i]; i++) {
-                    PyObject *valuestr;
-
-                    valuestr = LDAPberval_to_object(bvals[i]);
-                    if (PyList_Append(valuelist, valuestr) == -1) {
-                        Py_DECREF(pyattr);
-                        Py_DECREF(attrdict);
-                        Py_DECREF(result);
-                        Py_DECREF(valuestr);
-                        Py_DECREF(valuelist);
-                        if (ber != NULL)
-                            ber_free(ber, 0);
-                        ldap_msgfree(m);
-                        ldap_memfree(attr);
-                        ldap_memfree(dn);
-                        Py_XDECREF(pyctrls);
-                        return NULL;
-                    }
-                    Py_DECREF(valuestr);
-                }
-                ldap_value_free_len(bvals);
-            }
-            Py_DECREF(pyattr);
-            Py_DECREF(valuelist);
-            ldap_memfree(attr);
-        }
-
-        pydn = PyUnicode_FromString(dn);
-        if (pydn == NULL) {
-            Py_DECREF(result);
-            ldap_msgfree(m);
-            ldap_memfree(dn);
             return NULL;
         }
 
-        if (add_ctrls) {
-            entrytuple = Py_BuildValue("(OOO)", pydn, attrdict, pyctrls);
-        }
-        else {
-            entrytuple = Py_BuildValue("(OO)", pydn, attrdict);
-        }
-        Py_DECREF(pydn);
-        ldap_memfree(dn);
-        Py_DECREF(attrdict);
-        Py_XDECREF(pyctrls);
-        PyList_Append(result, entrytuple);
         Py_DECREF(entrytuple);
-        if (ber != NULL)
-            ber_free(ber, 0);
     }
+
     for (entry = ldap_first_reference(ld, m);
          entry != NULL; entry = ldap_next_reference(ld, entry)) {
         char **refs = NULL;
